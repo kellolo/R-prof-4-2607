e296acf4792481725bdeb1048211a42a82e90dc5
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-08-05 20:31:26 +0300

    style

--- ST/VitaliiKuroeDov/src/components/ChatList/ChatList.jsx
+++ ST/VitaliiKuroeDov/src/components/ChatList/ChatList.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-
+import { ListItem } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 export default class ChatList extends Component {
 
     render() {
         return(
             <aside className="chat-list">
-                <h3>Тут будут чаты</h3>
+                    <h3>Тут будут чаты</h3>
+                <ListItem>
+                </ListItem>
             </aside>
         )
     }
