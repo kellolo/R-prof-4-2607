8c06b33127ea310292f51a2efbe3e81da5f3f0bb
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-08-08 15:29:11 +0300

    sdasd

--- ST/VitaliiKuroeDov/src/components/ChatList/ChatList.jsx
+++ ST/VitaliiKuroeDov/src/components/ChatList/ChatList.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
 import { List } from '@material-ui/core'
 import ChatListItem from '../ChatListItem/ChatListItem'
-=======
-import { ListItem } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
->>>>>>> 4a2b3f845a1a602259c97ca8d14606f3bf898d04
 export default class ChatList extends Component {
 
     render() {
@@ -16,15 +11,9 @@ export default class ChatList extends Component {
 
         return(
             <aside className="chat-list">
-<<<<<<< HEAD
                 <List dense className="">
                     {chatsElemet.map((item, idx) => <ChatListItem key={item.id} {...item} />)}
                 </List>
-=======
-                    <h3>Тут будут чаты</h3>
-                <ListItem>
-                </ListItem>
->>>>>>> 4a2b3f845a1a602259c97ca8d14606f3bf898d04
             </aside>
         )
     }
